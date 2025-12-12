d52434075136c03a1571d65e200d87cf71ae9a24
miseon768-tech <miseon768@gmail.com>
2025-12-12 11:39:12 +0900

    accodationController, LikesController 기초작업 자리잡기

--- src/main/java/com/example/airbnb/mappers/LikesMapper.java
+++ src/main/java/com/example/airbnb/mappers/LikesMapper.java
@@ -1,7 +1,9 @@
 package com.example.airbnb.mappers;
 
+import com.example.airbnb.domain.entity.Likes;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface LikesMapper {
+    int insert(Likes like);
 }
