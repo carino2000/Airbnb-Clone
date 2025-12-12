cc037146debf4bd1c8c71d9c777c1186a8c5c437
miseon768-tech <miseon768@gmail.com>
2025-12-12 15:23:16 +0900

    숙소 수정 완성

--- src/main/java/com/example/airbnb/mappers/AccommodationMapper.java
+++ src/main/java/com/example/airbnb/mappers/AccommodationMapper.java
@@ -2,8 +2,14 @@ package com.example.airbnb.mappers;
 
 import com.example.airbnb.domain.entity.Accommodation;
 import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
 
 @Mapper
 public interface AccommodationMapper {
-    int insert(Accommodation accommodation);
+    int insertAccommodation(Accommodation accommodation);
+    int updateAccommodation(Accommodation accommodation);
+
+    List<Accommodation> selectAllAccommodations();
+    Accommodation selectAccommodationById(int accommodationId);
+
 }
